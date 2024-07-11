75d8ed4db4577d7b238702fc4794e3c59fee4971
Souchy <robyn.g@hotmail.fr>
2024-07-11 14:21:12 -0400

    CI/CD + Dockerisation (#12)
    * dockerfile done + wip workflows
    
    * wip workflows
    
    * python 3.11 + add labels on deploy + test deploy on pr
    
    * Update deploy.yaml
    
    * Update deploy.yaml
    
    * Update deploy.yaml
    
    * Update Dockerfile
    
    * Update deploy.yaml
    
    * Update deploy.yaml
    
    * Update deploy.yaml
    
    * test
    
    * Update deploy.yaml
    
    * Update deploy.yaml
    
    * Update deploy.yaml
    
    * test version
    
    * Update deploy.yaml
    
    * Update deploy.yaml
    
    * Update deploy.yaml
    
    * secret docker_image path
    
    * Create test_placeholder.py
    
    * Update deploy.yaml

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,14 @@
 ## To implement
+FROM python:3.11
+
+WORKDIR /app
+
+RUN pip install pipenv
+
+COPY Pipfile .
+RUN pipenv install
+
+# COPY .env .
+COPY src src
+
+ENTRYPOINT pipenv run start
---
+++ test/integration/test_placeholder.py
@@ -0,0 +1,11 @@
+# To Implement
+import unittest
+
+
+class TestIntegrationPlaceholder(unittest.TestCase):
+    def test(self):
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
